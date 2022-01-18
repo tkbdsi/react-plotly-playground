aed54c0ea5c5389e5557380de739f3a512dbe464
Tony Kelly <patient-remix-0q@icloud.com>
2022-01-18 14:18:00 -0500

    feat: add scaffold for simple plot

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
+import React from 'react';
+import Plot from 'react-plotly.js';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <React.Fragment>
+        <h2 style={{marginLeft: "20px"}}>Tony's Plotly Playground</h2>
+        <Plot data={[
+          {
+            x: [1, 2, 3],
+            y: [2, 6, 3],
+            type: 'scatter',
+            mode: 'lines+markers',
+            marker: {color: 'red'},
+          },
+        ]} />
+      </React.Fragment>
+    )
+  }
 }
+
 
 export default App;
