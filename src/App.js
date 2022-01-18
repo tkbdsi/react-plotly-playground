3a4e1697120bc027591a59a9841b3cd1c66646d9
Tony Kelly <patient-remix-0q@icloud.com>
2022-01-18 14:22:24 -0500

    feat: add Header component

--- src/App.js
+++ src/App.js
@@ -2,16 +2,23 @@ import React from 'react';
 import Plot from 'react-plotly.js';
 import './App.css';
 
+import Header from './components/Header';
+
 class App extends React.Component {
   constructor() {
     super();
-    this.state = {};
+    this.state = {
+      xdata: [],
+      ydata: [],
+
+    };
   }
 
   render() {
     return (
       <React.Fragment>
         <h2 style={{marginLeft: "20px"}}>Tony's Plotly Playground</h2>
+        <Header />
         <Plot data={[
           {
             x: [1, 2, 3],
---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Header = () => {
+
+  return(
+    <header style={{textAlign: 'center'}}>
+      <h2>Plotting Quantum Well Solutions</h2>
+    </header>
+  )
+}
+
+export default Header;
